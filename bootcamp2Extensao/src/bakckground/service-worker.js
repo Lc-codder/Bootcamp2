a7e49be430dfc9b32ba55d21aff82d3c3b6125af
Lucas Daniel <guimaraesslucas15@gmail.com>
2025-09-14 14:30:08 -0300

    Delete bootcamp2Extensao/src/bakckground directory

--- bootcamp2Extensao/src/bakckground/service-worker.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Bootcamp Helper instalado.');
-});
-
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === 'PING') {
-    sendResponse({ ok: true, time: new Date().toISOString() });
-  }
-});
